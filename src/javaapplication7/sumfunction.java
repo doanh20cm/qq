644c9240b885f02786e30816198ccdf3bac9c3d5
doanh20cm <doanhidm1@gmail.com>
2022-02-15 01:35:48 +0700

    add sum fx

---
+++ src/javaapplication7/sumfunction.java
@@ -0,0 +1,11 @@
+package JavaApplication7;
+
+
+public class sumfunction {
+    public static void main(String[] args) {
+       int a = 10;
+       int b = 20;
+       int c = a + b;
+       System.out.println("Sum of " + a + " and " + b + " is " + c);
+       }
+}
