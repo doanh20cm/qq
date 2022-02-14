8ed3970d1e1d409dce584d3250ba3a982ec07cd4
doanh20cm <97477998+doanh20cm@users.noreply.github.com>
2022-02-15 02:24:23 +0700

    change 10 to 11

--- src/javaapplication7/sumfunction.java
+++ src/javaapplication7/sumfunction.java
@@ -3,7 +3,7 @@ package JavaApplication7;
 
 public class sumfunction {
     public static void main(String[] args) {
-       int a = 10;
+       int a = 11;
        int b = 20;
        int c = a + b;
        System.out.println("Sum of " + a + " and " + b + " is " + c);
